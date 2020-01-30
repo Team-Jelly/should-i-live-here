f5819ae2e886d974a825bb1070b119b66c66cf71
Eli <eligallipoli317@gmail.com>
2020-01-30 15:20:42 -0500

    longitude and latitude in state

--- client/reducers/searchReducer.js
+++ client/reducers/searchReducer.js
@@ -4,6 +4,8 @@ const initialState = {
   address_search: '',
   previous_searches: [],
   current_results: null,
+  longitude: 40.71,
+  latitude: -74.00,
 };
 
 const searchReducer = (state = initialState, action) => {
@@ -14,6 +16,8 @@ const searchReducer = (state = initialState, action) => {
         address_search: action.payload.address_search,
         previous_searches: [...state.previous_searches, action.payload.address_search],
         current_results: action.payload.current_results,
+        longitude: action.payload.current_results[0].location.latitude,
+        latitude: action.payload.current_results[0].location.longitude,
       };
     case HISTORY:
       return {
