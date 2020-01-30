6dcb125d75e0f4526f3b3e968891d9e1507ea3f3
Connor Bovino <connor.bovino@gmail.com>
2020-01-30 16:00:34 -0500

    here

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -1,8 +1,8 @@
 const cookieController = {};
 
-// set cookie upon each visit to the root directory to store
-cookieController.setSSIDCookie = (req, res, next) => {
-  res.cookie('secret', res.locals.user.email);
-};
+// // set cookie upon each visit to the root directory to store
+// cookieController.setSSIDCookie = (req, res, next) => {
+//   return res.cookie('secret', res.locals.user.email);
+// };
 
 module.exports = cookieController;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,14 +3,13 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const searchHistoryController = require('../controllers/searchHistoryController');
-const cookieController = require('../controllers/cookieController');
 
 
 // route & middleware for registering a new user
-router.post('/register', userController.createUser, cookieController.setSSIDCookie, (req, res) => res.status(200).json(res.locals.user));
+router.post('/register', userController.createUser, (req, res) => res.status(200).json(res.locals.user));
 
 // route & middleware for verifying a user login attempt
-router.post('/login', userController.verifyUser, cookieController.setSSIDCookie, (req, res) => res.status(200).json(res.locals.user));
+router.post('/login', userController.verifyUser, (req, res) => res.status(200).json(res.locals.user));
 
 router.get('/history/:user', searchHistoryController.getHistory, (req, res) => res.status(200).json(res.locals.history));
 
